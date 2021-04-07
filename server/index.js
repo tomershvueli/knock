4f4a168c041e9286ad8ccdc05ec71663e1a14ca7
Tomer Shvueli <tomer@shvueli.com>
2021-04-07 16:47:10 -0400

    feat: Basic error handling

--- server/index.js
+++ server/index.js
@@ -9,28 +9,42 @@ const { getRepos } = require("../util/github");
 // Define routes
 
 server.get("/v1/google/repositories", async (req, res) => {
-  const repos = await getRepos();
+  try {
+    const repos = await getRepos();
 
-  res.json(repos);
+    res.json(repos);
+  } catch (err) {
+    // Be sure to log the error somewhere appropriate and notify code owners
+    res.status(500).send({
+      message: "There was a problem with your request"
+    });
+  }
 });
 
 // https://stackoverflow.com/a/59690398/11646872
 server.put("/v1/google/repositories/to_file", async (req, res) => {
-  const repos = await getRepos();
+  try {
+    const repos = await getRepos();
 
-  const gz = zlib.createGzip();
-  gz.on("error", () => {
-    return res.status(500).send({
-      error: "Problem writing to file"
+    const gz = zlib.createGzip();
+    gz.on("error", () => {
+      return res.status(500).send({
+        error: "Problem writing to file"
+      });
     });
-  });
 
-  const writeStream = fs.createWriteStream("/tmp/knock_interview.json.gz");
-  gz.pipe(writeStream);
-  gz.write(JSON.stringify(repos));
-  gz.end();
+    const writeStream = fs.createWriteStream("/tmp/knock_interview.json.gz");
+    gz.pipe(writeStream);
+    gz.write(JSON.stringify(repos));
+    gz.end();
 
-  res.sendStatus(200);
+    res.sendStatus(200);
+  } catch (err) {
+    // Be sure to log the error somewhere appropriate and notify code owners
+    res.status(500).send({
+      message: "There was a problem with your request"
+    });
+  }
 });
 
 server.get("*", (req, res) => {
